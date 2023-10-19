490d773eaa15f98f09ca57d7cf5cc2b6dcdde196
NightmareScan <148421902+NightmareScan@users.noreply.github.com>
2023-10-18 23:38:02 -0600

    Initial commit

---
+++ flamescans.org/wp-content/themes/mangareader/assets/js/tsmedia8a54.js
@@ -0,0 +1,2 @@
+function tsMedia(x){if(x.matches){jQuery("#titlemove").appendTo(jQuery("#mobiletitle"));}else{jQuery("#titlemove").prependTo(jQuery("#titledesktop"));}}
+var tsmmedia=matchMedia("(max-width: 800px)");tsmmedia.addListener(tsMedia);
